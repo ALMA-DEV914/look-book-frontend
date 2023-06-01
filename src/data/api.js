b1abd3bfd5d1fbd804b1314a07cc37d670aa7c31
Alma Braun <aungonalna58@gmail.com>
2023-06-01 12:22:21 -0700

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -2,6 +2,19 @@ import axios from 'axios';
 
 const apiUrl =  "https://look-book.onrender.com/api/";
 
+//defining mongoose options
+const options = {
+  origin: ["http://localhost:3000" , "https://look-book.onrender.com/"],
+  credentials: "include",
+  header: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Content-type":"application/json",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+};
+
+
 
 export const singleFileUpload = async (data, options) => {
     try {
